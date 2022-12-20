10034d83a33aae75b4ba11802cb813f37b113b98
Felipe S. Neves <felipeneves.github@gmail.com>
2022-12-20 00:05:29 -0300

    Adicionando usuário root para teste

--- sql/2_tb_usuarios.sql
+++ sql/2_tb_usuarios.sql
@@ -5,3 +5,6 @@ CREATE TABLE usuarios (
     senha VARCHAR(32) NOT NULL, 
     PRIMARY KEY(id_usuario)
 );
+
+-- SENHA DEFAULT: 123
+INSERT INTO usuarios (nome, email, senha) VALUES ('root', 'root@teste.com.br', '202cb962ac59075b964b07152d234b70');
